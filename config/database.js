1671fdc53238cfd9ce52e3f6922459f2a450533a
mustafaegilmez <21010310053@ogrenci.bartin.edu.tr>
2024-12-06 01:31:41 +0300

    gitignore database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize('smart_monitoring_system_db', 'root', 'dogukan1903', {
+const sequelize = new Sequelize('smart_monitoring_system_db', 'root', 'vgzlGya9nIEHKv9', {
 
     host: 'localhost',
     dialect: 'mysql',
